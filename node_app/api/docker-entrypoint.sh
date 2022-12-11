f05ab717563f6dcbf02b355959055310401cc7bc
Aleksandr Melanchenko <alexnd@gmail.com>
2022-12-11 18:05:40 +0200

    update with SSE example

--- node_app/api/docker-entrypoint.sh
+++ node_app/api/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-if [[ ! -f /app/.env ]]
-then
+if [ ! -f /app/.env ]; then
     cp -f /etc/app.api.env /app/.env
     #cp -f /app/env.template /app/.env
     #sed -i "s/API_PORT=/API_PORT=$API_PORT/" /app/.env
--- node_app/src/app.js
+++ node_app/src/app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const PORT = process.env.API_PORT || 3000;
+const LOOP_TIMEOUT = 4500;
+const data = {ts:0, dt:0};
+const listeners = [];
 
 const server = http.createServer(async (req, res) => {
     if (req.url === '/api/test' && req.method === 'GET') {
@@ -7,6 +10,22 @@ const server = http.createServer(async (req, res) => {
         var ts = Date.now();
         res.write(`{"data":"API Resource ${ts}"}`);
         res.end();
+    } else if (req.url === '/api/stream' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Connection': 'keep-alive',
+            'Content-Type': 'text/event-stream',
+            'Access-Control-Allow-Origin': '*',
+        });
+        res.on('close', () => {
+            listeners.forEach((listener, i) => {
+                if (listener === res) {
+                    console.log('*clean listener')
+                    listeners.splice(i, 1);
+                }
+            });
+            res.end();
+        });
+        listeners.push(res);
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Route not found' }));
@@ -16,3 +35,23 @@ const server = http.createServer(async (req, res) => {
 server.listen(PORT, () => {
     console.log(`API server started on port: ${PORT}`);
 });
+
+update();
+
+function update() {
+    let ts = Date.now();
+    data._dt = data.dt;
+    data._ts = data.ts;
+    data.dt = ts - data.ts;
+    data.ts = ts;
+    console.log('*update', data.ts);
+    updateListeners();
+    setTimeout(update, LOOP_TIMEOUT);
+}
+
+function updateListeners() {
+    listeners.forEach(res => {
+        const message = {ts: data.ts, dt: data.dt};
+        res.write(`message: serverUpdate\ndata: ${JSON.stringify(message)}\n\n`);
+    });
+}
